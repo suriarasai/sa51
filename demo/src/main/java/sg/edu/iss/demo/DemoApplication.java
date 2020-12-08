68d6415f040a6a5205eda6aa417f6058fc2d68ae
Suria R Asai <suria.r.asai@gmail.com>
2020-12-08 17:15:07 +0800

    demo

--- demo/src/main/java/sg/edu/iss/demo/DemoApplication.java
+++ demo/src/main/java/sg/edu/iss/demo/DemoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-
 public class DemoApplication {
 	
 		
