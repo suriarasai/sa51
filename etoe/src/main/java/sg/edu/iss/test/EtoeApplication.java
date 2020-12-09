e7c38711206337527d2344d75abfe83cfcbb0904
Suria R Asai <suria.r.asai@gmail.com>
2020-12-09 10:58:00 +0800

    First Commit for End to End Project

---
+++ etoe/src/main/java/sg/edu/iss/test/EtoeApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtoeApplication.class, args);
+	}
+
+}
---
+++ etoe/src/test/java/sg/edu/iss/test/EtoeApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
