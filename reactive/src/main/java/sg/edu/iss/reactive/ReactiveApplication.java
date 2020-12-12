7e1e6e1aa1750101f593402ab2ab9714fc1d0889
Suria R Asai <suria.r.asai@gmail.com>
2020-12-12 11:20:39 +0800

    Reactive Demo

---
+++ reactive/src/main/java/sg/edu/iss/reactive/ReactiveApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApplication.class, args);
+	}
+
+}
---
+++ reactive/src/test/java/sg/edu/iss/reactive/ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.iss.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
